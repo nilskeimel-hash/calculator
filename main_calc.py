22687e05ce83dd2c99fe3efb4b8dfd0dca8c7695
Nils Keimel <nils.keimel@outlook.com>
2026-07-21 15:06:20 +0200

    dictcomp, history file

--- main_calc.py
+++ main_calc.py
@@ -1,5 +1,16 @@
 import calculator
 
+def log_history(line: str) -> None:
+    with open("history.txt", "a") as history_file:
+        history_file.write(line + "\n")
+
+def read_history() -> list[str]:
+    try:
+        with open("history.txt", "r") as history_file:
+            return [line.strip() for line in history_file.readlines()]
+    except FileNotFoundError:
+        return []
+
 TWO_NUMBER_OPS = {
     "+": calculator.plus,
     "-": calculator.minus,
@@ -23,11 +34,17 @@ ONE_NUMBER_OPS = {
 
 INTEGER_OPS = {"!": calculator.factorial}
 
+COMMAND_OPS = {
+    "history" : read_history,
+}
+
+ALLE_OPS = TWO_NUMBER_OPS | ONE_NUMBER_OPS | INTEGER_OPS | COMMAND_OPS
+
 
 while True:
     print("Welcome to the calculator!")
     print(
-        f"Available operations: {', '.join(list(TWO_NUMBER_OPS.keys()) + list(ONE_NUMBER_OPS.keys()) + list(INTEGER_OPS.keys()))}"
+        f"Available operations: {', '.join(ALLE_OPS)}"
     )
     operation = input("Enter an operation (or 'exit' to quit): ")
     if operation.lower() == "exit":
@@ -38,6 +55,7 @@ while True:
             a = float(input("Enter the first number: "))
             b = float(input("Enter the second number: "))
             result = TWO_NUMBER_OPS[operation](a, b)
+            log_history(f"{a} {operation} {b} = {result}")
             print(f"The result is: {result}")
         except ValueError as e:
             print(e)
@@ -46,6 +64,7 @@ while True:
         try:
             a = float(input("Enter a number: "))
             result = ONE_NUMBER_OPS[operation](a)
+            log_history(f"{operation} of {a} = {result}")
             print(f"The result is: {result}")
         except ValueError as e:
             print(e)
@@ -54,9 +73,19 @@ while True:
         try:
             n = int(input("Enter an integer: "))
             result = INTEGER_OPS[operation](n)
+            log_history(f"{n}{operation} = {result}")
             print(f"The result is: {result}")
         except ValueError as e:
             print(e)
             continue
+    elif operation in COMMAND_OPS:
+        lines = COMMAND_OPS[operation]()
+        if lines:
+            print("History:")
+            for line in lines:
+                print(line)
+        else:
+            print("No history found.")
+            
     else:
         print("Invalid operation. Please try again.")
